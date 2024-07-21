e22fcaaf53421e554010b8e3c14002ced4874e1f
pantha704 <prathamjaiswal204@gmail.com>
2024-07-21 08:14:37 +0530

    .

--- testing-rust/src/main.rs
+++ testing-rust/src/main.rs
@@ -148,9 +148,23 @@ fn main() {
     for element in arr.iter() {
         println!("Element : {}",element);
     }
-    
+
     for num in (1..4).rev() {    // 1 to 3 ( 4 excluded ) and .rev() to reverse it into 3 to 1.
         println!("{}!", num);  // OP : 3! 2! 1!
+    }
+
+    for num in 1..=100 {
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("FizzBuzz");
+        } else if num % 3 == 0 {
+            println!("Fizz");
+        } else if num % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}",number);
+        }
+
+        }
     }
 
 }   
