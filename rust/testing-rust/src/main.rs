db2c79d918d78771bff78af138849ab0cb331b65
pantha704 <prathamjaiswal204@gmail.com>
2024-09-14 13:16:54 +0530

    .

---
+++ rust/testing-rust/src/main.rs
@@ -0,0 +1,173 @@
+/*
+
+cargo init       // for initialising a rust project ( just like npx create-vite/next )
+cargo check      // to check for pre-compiling errors ( errors before compilation )
+cargo build      // to compile the rust program
+cargo run        // to compile and run the rust program
+
+tuto in yt : https://youtube.com/playlist?list=PLPoSdR46FgI412aItyJhj2bF66cudB6Qs&si=46PxwHWNpejJ5JRF
+
+*/
+
+fn main() {
+    println!("Hello, world!");
+
+    // Sleep for 3 seconds
+    // std::thread::sleep(std::time::Duration::from_secs(3));
+
+    // Defining variables
+    let mut x: i32 = 3;     // by default "let" variables are immutable add "mut" keyword after them to make them mutable.
+    x = 4;
+    println!("Value of x {}",x);   // OP: 4 
+
+    let mut x = "dad";  // double quotes store "strings" , single quotes store 'chars'. Might come handy in saving a few bits of space.
+    x = "f";                  // 'f' is a char and "f" is string. mut doesnt allow the change of data types.
+    println!("Value of x {}",x);   // OP: f  (string)
+
+    
+    // Shadowing 
+    let x = 5;    // Original value
+    println!("Value of x {}",x);   // OP: 5
+
+    let x = 'r';    // Shadowed value
+    println!("Value of x {}",x);   // OP: r  (char)
+
+    /* 
+    Redeclaring same variable with let is called shadowing,
+    which allows us to redefine "value" and "data type" of the variable.
+
+
+    while, "mut" keyword allows us only to change the value not the data type. 
+    */
+
+    const PI: f32 = 3.14159265359;
+    println!("Value of PI {}",PI);
+
+    // CHARS
+    for char in "Hello".chars() {
+        // println!("{}",char);
+    }
+// Compund Data Tyoes:
+    // TUPLES   Collection of dynamic data types
+    let tuple: (i32, f64, char) = (500, 6.4, 'X');
+    
+    let five_hundred = tuple.0;
+    let six_point_four = tuple.1;
+    let x = tuple.2;
+    println!("Value of Five Hundred is : {}",five_hundred);
+    println!("Value of Six Point Four is : {}",six_point_four);
+    println!("Value of x is : {}",x);
+
+    // ARRAYS   Collection of same data types
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let first = arr[0];
+    let second = arr[1];
+
+    println!("first : {}, second : {}",first,second);
+
+    for element in arr.iter() {
+        // println!("element : {}",element);
+    }
+
+// Custom Data Types
+    // STRUCTS
+   struct Person {
+      name: String,
+      age: u8,
+    }
+
+    let person = Person {
+        name: String::from("John"),
+        age: 20,
+    };
+
+    println!("The person's name is {}, his age is : {}",person.name,person.age);
+
+    // ENUMS 
+    enum TrafficLight {
+        Red,
+        Yellow,
+        Green,
+    }
+
+    let light = TrafficLight::Red;
+
+    match light {
+        TrafficLight::Red => println!("Stop!"),
+        TrafficLight::Yellow => println!("Slow down!"),
+        TrafficLight::Green => println!("Go!"),
+    };
+
+    let x = sum_diff(2,4);
+    fn sum_diff(n1: i32, n2: i32) -> (i32, i32) {
+      (n1+n2, n1-n2)
+    }
+    print!("Sum: {}, Difference: {},\t",x.0,x.1);
+    println!("As a raw Tuple : {:?}",x);      // {:?}  is used to print a tuple/array or {:#?} for pretty print.
+
+
+    // Control Flow in Rust
+
+    let num = if 2 > 1 {
+        true
+    } else {
+        false
+    };
+
+    println!("Output : {}",num);   // OP : true
+
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_of_cent(coin : Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    let coin = Coin::Penny;
+
+    println!("Value of coin : {}",value_of_cent(coin));
+
+    // 'loop' runs an infinite loop until the 'break' keyword is called from inside it.
+    loop { 
+        println!("Yo!  ");
+        break;
+    }
+
+    let arr = [1,2,3,4];
+    for element in arr.iter() {
+        println!("Element : {}",element);
+    }
+
+    for num in (1..4).rev() {    // 1 to 3 ( 4 excluded ) and .rev() to reverse it into 3 to 1.
+        println!("{}!", num);  // OP : 3! 2! 1!
+    }
+
+    for num in 1..=100 {
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("FizzBuzz");
+        } else if num % 3 == 0 {
+            println!("Fizz");
+        } else if num % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", num);
+        }
+    }
+
+    // Ownerships in Rust
+
+    
+
+
+}   
