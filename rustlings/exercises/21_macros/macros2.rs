9f494c41b6ffe324df499ae1714dcd3c55e1f177
pantha704 <prathamjaiswal204@gmail.com>
2025-01-28 23:02:41 +0530

    completed_macros

--- rustlings/exercises/21_macros/macros2.rs
+++ rustlings/exercises/21_macros/macros2.rs
@@ -1,9 +1,10 @@
+// TODO: Fix the compiler error by moving the whole definition of this macro.
 macro_rules! my_macro {
-    // TODO: Fix the compiler error by moving the whole definition of this macro.
     () => {
         println!("Check out my macro!");
     };
 }
+
 fn main() {
     my_macro!();
 }
